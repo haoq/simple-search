f23ae2fe5f29edf5959ed99bf1bb0d86f6732b7f
haoq <haoq@uw.edu>
2014-11-01 15:06:40 +0800

    no message

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -21,7 +21,7 @@ $(function(){
 function search(query, $container, $template){
     $.ajax({
         type: 'GET',
-        url: 'http://128.95.157.44:8080/solr-example/collection1/select',
+        url: 'http://is-info320t1.ischool.uw.edu:8080/solr-example/collection1/select',
         dataType: 'JSONP',
         data: {
             'q': query,
